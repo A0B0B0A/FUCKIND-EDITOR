57e90a41a90e542140f6862f71d3e98c792386fa
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-02-25 17:24:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QInputDialog
 from ui import Ui_MainWindow
 
 class Widget(QMainWindow):
@@ -8,25 +8,63 @@ class Widget(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.notes = {
-            "Перша замітка":{
-                "Текст": "Це текст",
-                "теги": []
-            }
-        }
+        self.read_notes()
         self.ui.notes.addItems(self.notes)
         self.ui.notes.itemClicked.connect(self.show_note)
         self.ui.save_note.clicked.connect(self.Savednote)
+        self.ui.create_notes.clicked.connect(self.creatednote)
+        self.ui.delete_notes.
 
     def show_note(self):
         self.name = self.ui.notes.selectedItems()[0].text()
         self.ui.text.setText(self.notes[self.name]["Текст"])
 
     def Savednote(self):
-        self.notes[self.name] = self.ui.text.toPlainText()
+        self.notes[self.name] = {
+                "Текст": self.ui.text.toPlainText(),
+                "теги": []
+            }
         with open("note.json", "w", encoding = "utf-8") as file:
             json.dump(self.notes, file)
+        self.ui.notes.clear()
+        self.ui.notes.addItems(self.notes)
         
+
+    def clearnote(self):
+        self.ui.text.clear()
+
+    def creatednote(self):
+        self.clearnote()
+        note_name, ok = QInputDialog.getText(notes_win, "Додати замітку", "Назва замітки: ")
+        if ok and note_name != "":
+            self.name = note_name
+
+    def read_notes(self):
+        try:
+            with open("note.json", "r", encoding="utf-8") as file:
+                self.notes = json.load(file)
+        except:
+            self.notes = {
+                    "Перша замітка":{
+                        "Текст": "Це текст",
+                        "теги": []
+                    }
+                }
+
+    def deletenote(self):
+        try:
+            del self.notes[self.name]
+            self.clearnote()
+            self.ui.notes.clear()
+            self.ui.notes.addItems(self.notes)
+            self.Savednote()
+        except:
+            print("Помилка видалення")
+        
+
+
+
+            
 
 
 
